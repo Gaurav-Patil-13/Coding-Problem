e1c17e2fbe03a9f880204fd7b99bddf15437c566
Gaurav Patil <patilgaurav5634@gmail.com>
2026-07-31 18:46:14 +0530

    Time: 92 ms (6.3%), Space: 14.6 MB (37.47%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -40,7 +40,7 @@ public:
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m; j++){
-                if( !vis[i][j]){
+                if(board[i][j] == 'O' && !vis[i][j]){
                     board[i][j] ='X';
                 }
             }
